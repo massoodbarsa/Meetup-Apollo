305f727fb700c191a6a4a800c9815dd57fc32fc2
Amir Shakiba Nasab <amir.shakiba1980@gmail.com>
2021-07-12 19:43:09 +0200

    add slider to userProfile - extract data

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { UserContext } from '../context/UserContextProvider'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBirthdayCake } from '@fortawesome/free-solid-svg-icons'
+import './UserProfile.scss'
+import Carousel from 'react-bootstrap/Carousel';
 
 export default function UserProfile() {
 
     const context = useContext(UserContext)
-
-    console.log(context.usersData);
-
 
     const { email } = useParams()
 
@@ -18,11 +18,60 @@ export default function UserProfile() {
 
     })
 
-    console.log(user);
-    console.log(email);
+    const { age, aboutMe, gender, height, place, photos, profilePhoto, surename, name } = user[0]
+
+
     return (
-        <div>
-            
+        <div className='userProfile-container'>
+            <div className='userProfile-container__images'>
+                {
+                    <Carousel slide={true}>
+                        {
+                            photos.map((item, index) => {
+                                const { url } = item
+
+                                return (
+                                    profilePhoto &&
+                                    <Carousel.Item key={index} className=''>
+                                        <Carousel.Caption>
+                                            <h1>{name}</h1>
+                                        </Carousel.Caption>
+                                            <img
+                                                className="section-center__img"
+                                                src={`http://localhost:4000${url} `}
+                                                alt="First slide"
+
+                                            />
+                                    </Carousel.Item>
+                                )
+                            })
+                        }
+                    </Carousel>
+                }
+            </div>
+            <div className='userProfile-container__info'>
+                <section className='userProfile-container__info__section'>
+                    <FontAwesomeIcon icon={faBirthdayCake} size='3x' />
+                    <p>{age}</p>
+                </section>
+                <section className='userProfile-container__info__section'>
+                    <FontAwesomeIcon icon={faBirthdayCake} size='3x' />
+                    <p>{age}</p>
+                </section>
+                <section className='userProfile-container__info__section'>
+                    <FontAwesomeIcon icon={faBirthdayCake} size='3x' />
+                    <p>{age}</p>
+                </section>
+                <section className='userProfile-container__info__section'>
+                    <FontAwesomeIcon icon={faBirthdayCake} size='3x' />
+                    <p>{age}</p>
+                </section>
+                <section className='userProfile-container__info__section'>
+                    <FontAwesomeIcon icon={faBirthdayCake} size='3x' />
+                    <p>{age}</p>
+                </section>
+            </div>
+
         </div>
     )
 }
